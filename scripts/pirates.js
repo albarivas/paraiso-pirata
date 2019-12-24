77113f6e60abb2e0ada0732dce4be6a4d7b49b4b
Alba Rivas <alba.azconarivas@gmail.com>
2019-12-24 11:56:41 +0100

    Initial commit

---
+++ scripts/pirates.js
@@ -0,0 +1,253 @@
+const pirates = {
+  common: {
+    'Pata Palo': {
+      ability: 'Pata Metal',
+      life: 30,
+      attack: 15,
+      attackrounds: 1
+    },
+    'Capitán Ganchillo': {
+      ability: 'Gancho Trampa',
+      life: 50,
+      attack: 20,
+      attackrounds: 1
+    },
+    'El Parcheado': {
+      ability: 'Parches Reparcheados',
+      life: 40,
+      attack: 0, //heals 15 life to a teammate or himself
+      attackrounds: 1
+    },
+    'Jugo de Barbadilla': {
+      ability: 'Punzada Jugosa',
+      life: 50,
+      attack: 15,
+      attackrounds: 1
+    },
+    'Timón Timado': {
+      ability: 'Timón Afilado',
+      life: 50,
+      attack: 20,
+      attackrounds: 1
+    },
+    'Cañónigus': {
+      ability: 'Arreglo Cañónigo',
+      life: 40,
+      attack: 15,
+      attackrounds: 1
+    },
+    'Capitán Tontainus Barquinhus': {
+      ability: 'Inteligencia Artificial',
+      life: 30,
+      attack: 30,
+      attackrounds: 2
+    },
+    'Cazatesoros': {
+      ability: 'Garfio Doble',
+      life: 40,
+      attack: 20,
+      attackrounds: 1
+    },
+    'Catalejo Polémico': {
+      ability: 'Polémica Disipada',
+      life: 45,
+      attack: 15,
+      attackrounds: 1
+    },
+    'El Náufrago': {
+      ability: 'Naufragio Invertido',
+      life: 40,
+      attack: 30,
+      attackrounds: 2
+    },
+    'Capitán Plastificado': {
+      ability: 'Plastificación Ciega',
+      attack: 10,
+      attackrounds: 1
+    },
+    'Capitán Extremioxidado': {
+      ability: 'Regresión Desoxidada',
+      life: 40,
+      attack: 15,
+      attackrounds: 1
+    },
+    'Capitán Loro': {
+      ability: 'Loro Reforzado',
+      life: 50,
+      attack: 20,
+      attackrounds: 1
+    },
+    'Capitán Ignoro': {
+      ability: 'Consejo Anticipado',
+      life: 70,
+      attack: 0, //gives advice to your team
+      attackrounds: 1
+    },
+    'Pila de Matal': {
+      ability: 'Matal Matadol',
+      life: 60,
+      attack: '10 x 2',
+      attackrounds: 1
+    }
+  },
+  lesscommon: {
+    'Pirata Garrapata': {
+      ability: 'Garrapata Plagada',
+      life: 75,
+      attack: 25,
+      attackrounds: 1
+    },
+    'Capitán Pata de Hierro': {
+      ability: 'Fuerza Herrática',
+      life: 60,
+      attack: 25,
+      attackrounds: 1
+    },
+    'Punto de Mira': {
+      ability: 'Mira Letal',
+      life: 70,
+      attack: 20,
+      attackrounds: 1
+    },
+    'Capitán Gorrexplosivo': {
+      ability: 'Gorroexplosión Mejorada',
+      life: 60,
+      attack: 20,
+      attackrounds: 1
+    },
+    'Bumeránicus': {
+      ability: 'Bumerán Masivo',
+      life: 65,
+      attack: 20,
+      attackrounds: 1
+    },
+    'Ratapi Léxicodis': {
+      ability: 'Construcción Acelerada',
+      life: 60,
+      attack: 0, //restores 50 life to the boat
+      attackrounds: 2
+    },
+    'Capitán Minado': {
+      ability: 'Mina Globocida',
+      life: 70,
+      attack: 50,
+      attackrounds: 2
+    },
+    'Capitán Coraza Perforada': {
+      ability: 'Coraza Restaurada',
+      life: 80,
+      attack: 25,
+      attackrounds: 1
+    }
+  },
+  special: {
+    'El Cañonazos': {
+      ability: 'Perdigón de la Muerte',
+      life: 100,
+      attack: '30 x 2', //deals 30 damage to 2 enemies
+      attackrounds: 1
+    },
+    'Barbarroja': {
+      ability: 'Latigazo Mortífero',
+      life: 80,
+      attack: 25,
+      attackrounds: 1
+    },
+    'Construcciones Pirantásticas': {
+      ability: 'Reinversión del Tiempo',
+      life: 125,
+      attack: 0, //restores 150 life to the boat
+      attackrounds: 3
+    },
+    'Submarata': {
+      ability: 'Locura Embestial',
+      life: 90,
+      attack: 25,
+      attackrounds: 1
+    },
+    'Curánticus Medianus': {
+      ability: 'Curánticus Gigánticus',
+      life: 70,
+      attack: 0, //heals 1 teammate 30 life
+      attackrounds: 1
+    },
+    'DJ Piramix': {
+      ability: 'Fiesta de 200 dB',
+      life: 120,
+      attack: '10 x 6', //deals 10 attack to all foe's objectives
+      attackrounds: 1
+    },
+    'Propaestriba': {
+      ability: 'Velocidad de la Luz',
+      life: 100,
+      attack: 35,
+      attackrounds: 1
+    }
+  },
+  epic: {
+    'Capitán Barbanegra': {
+      ability: 'Cañón Doble',
+      life: 175,
+      attack: 40,
+      attackrounds: 1
+    },
+    'Capitán Calavera': {
+      ability: 'Fuerza Herrática',
+      life: 150,
+      attack: 30,
+      attackrounds: 1
+    },
+    'Tacañares': {
+      ability: 'Tenaza Retorcida',
+      life: 170,
+      attack: 50,
+      attackrounds: 2
+    },
+    'Estrellatus': {
+      ability: 'Estrellatón',
+      life: 180,
+      attack: 40,
+      attackrounds: 1
+    },
+    'Bucanero': {
+      ability: 'Traición Bucanera',
+      life: 200,
+      attack: 40,
+      attackrounds: 1
+    }
+  },
+  legendary: {
+    'Comanpitán': {
+      ability: 'Comando Comandado',
+      life: 400,
+      attack: 30,
+      attackrounds: 1
+    },
+    'Pirabot': {
+      ability: 'Locura Robótica',
+      life: 300,
+      attack: '30 x 2 / 60', //this attack can be configurated to hit 1 or 2 enemies. If it's 1, damage will be 60, and if it's 2, damage will be 30
+      attackrounds: 1
+    },
+    'Myrakles': {
+      ability: 'Milagro Secundario',
+      life: 300,
+      attack: '?', //suma del ataque de tu equipo//
+      attackrounds: 1
+    }
+  },
+  mythical: {
+    'Holandés Errante': {
+      ability: 'Explosión Umbría',
+      life: 350,
+      attack: '50 x 6', //attacks all the foe's objectives
+      attackrounds: 2
+    },
+    'Pirratatatatatata': {
+      ability: 'Ametralladora Controlada',
+      life: 300,
+      attack: '10 x 8', //attacks one enemy 8 times in 1 attack
+      attackrounds: 1
+    }
+  }
+};
---
+++ scripts/tablero.js
@@ -0,0 +1,148 @@
+/* Load pirates.js before */
+
+let selectedCell;
+const colors = {
+    'common' : 'grey',
+    'lesscommon' : 'green',
+    'special' : 'blue',
+    'epic' : 'purple',
+    'legendary' : 'orange',
+    'mythical' : 'red'
+};
+
+function onAddPirateClick(cell) {
+    // Store selected cell for later
+    this.selectedCell = cell;
+
+    // Hide game tracking table
+    const gameTable = document.getElementById('game-tracking');
+    gameTable.style.display = 'none';
+
+    // Show quality election table
+    const electionTable = document.getElementById('quality-election');
+    electionTable.style.display = 'block';
+}
+
+function onQualityChosen(quality) {
+    // Delete pirates from pirate election table, if there are any from previous selections
+    const pirateElectionTable = document.getElementById('pirate-election');
+    while (pirateElectionTable.firstChild) {
+        pirateElectionTable.removeChild(pirateElectionTable.firstChild);
+    }
+
+    // Populate pirate election table with selected quality pirates
+    const piratesToShow = Object.keys(pirates[quality]);
+    let counter = 0;
+    let tr;
+    // Iterate over each pirate to add
+    piratesToShow.forEach((piratename) => {
+        // If multiple of 3, add a new row
+        if (counter % 3 === 0) {
+            tr = document.createElement('tr');
+            pirateElectionTable.appendChild(tr);
+        }
+        // Create pirate cell 
+        const td = document.createElement('td');
+        td.innerText = piratename;
+        td.addEventListener('click', () => {
+            onPirateSelected(quality, piratename);
+        });
+        td.style.backgroundColor = colors[quality];
+        tr.appendChild(td);
+        counter++;
+    });
+
+    // Hide quality election table
+    const qualityElectionTable = document.getElementById('quality-election');
+    qualityElectionTable.style.display = 'none';
+
+    // Show pirate election table
+    pirateElectionTable.style.display = 'block';
+}
+
+function onPirateSelected(quality, piratename) {
+    addSelectedPirateToGameTrackingCell(quality, piratename);
+
+    // Hide pirate election table
+    const pirateElectionTable = document.getElementById('pirate-election');
+    pirateElectionTable.style.display = 'none';
+
+    // Show game tracking table
+    const gameTable = document.getElementById('game-tracking');
+    gameTable.style.display = 'block';
+}
+
+function addSelectedPirateToGameTrackingCell(quality, piratename) {
+    const gameTrackingTd = this.selectedCell.parentElement;
+
+    // Remove any previously selected pirates
+    while (gameTrackingTd.firstChild) {
+        gameTrackingTd.removeChild(gameTrackingTd.firstChild);
+    }
+
+    // Get pirate info from pirates catalogue
+    const pirate = pirates[quality][piratename];
+
+    // Add selected pirate to game tracking selected cell
+    const name = document.createElement('p');
+    name.innerText = `Nombre: ${piratename}`;
+    gameTrackingTd.appendChild(name);
+
+    const ability = document.createElement('p');
+    ability.innerText = `Habilidad: ${pirate.ability}`;
+    gameTrackingTd.appendChild(ability);
+
+    const life = document.createElement('p');
+    const lifeInput = document.createElement('input');
+    lifeInput.type = 'number';
+    lifeInput.value = pirate.life;
+    life.innerText = `Vida:`;
+    life.appendChild(lifeInput);
+    gameTrackingTd.appendChild(life);
+
+    const attack = document.createElement('p');
+    const attackInput = document.createElement('input');
+    attackInput.type = 'text';
+    attackInput.value = pirate.attack;
+    attack.innerText = `Ataque:`;
+    attack.appendChild(attackInput);
+    gameTrackingTd.appendChild(attack);
+
+    const attackrounds = document.createElement('p');
+    const attackroundsInput = document.createElement('input');
+    attackroundsInput.type = 'number';
+    attackroundsInput.value = pirate.attackrounds;
+    attackrounds.innerText = `Rondas por Ataque:`;
+    attackrounds.appendChild(attackroundsInput);
+    gameTrackingTd.appendChild(attackrounds);
+
+    const remove = document.createElement('button');
+    remove.innerText = 'Borrar';
+    remove.addEventListener('click', (event) => {
+        onRemovePirate(event);
+    });
+    gameTrackingTd.appendChild(remove);
+
+    // Set correct background color
+    gameTrackingTd.style.backgroundColor = colors[quality];
+}
+
+function onRemovePirate(event) {
+    // Remove all elements from parent td
+    const clickedTd = event.target.parentElement
+    while (clickedTd.firstChild) {
+        clickedTd.removeChild(clickedTd.firstChild);
+    }
+
+    // Add "add pirate" button and reset color
+    const addPirate = document.createElement('button');
+    addPirate.innerText = 'Añadir Pirata';
+    addPirate.addEventListener('click', (event) => {
+        onAddPirateClick(event.target);
+    });
+    clickedTd.appendChild(addPirate);
+    clickedTd.style.backgroundColor = 'tan';
+
+    // Stop event propagation
+    event.stopImmediatePropagation();
+}
